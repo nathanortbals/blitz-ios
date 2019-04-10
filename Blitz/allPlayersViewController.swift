11d18d50bd5344a22f4a3f369a687400f2b5fb08
Aaron Henry <ahx97@mail.missouri.edu>
2019-04-10 15:13:37 -0500

    more work on UI
    Basic outline should be done now, need to not allo you to go back when you add a player

---
+++ Blitz/allPlayersViewController.swift
@@ -0,0 +1,49 @@
+//
+//  allPlayersViewController.swift
+//  Blitz
+//
+//  Created by Aaron Henry on 4/10/19.
+//  Copyright © 2019 nathanortbals. All rights reserved.
+//
+
+import UIKit
+
+class allPlayersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var allPlayerTableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "playerCell", for: indexPath) as! allPlayersTableViewCell
+        
+        cell.playerName.text = "Tom Brady"
+        cell.playerSalary.text = "14000000000"
+        cell.pos.text = "QB"
+        cell.matchup.text = "ATL"
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        allPlayerTableView.delegate = self
+        allPlayerTableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
